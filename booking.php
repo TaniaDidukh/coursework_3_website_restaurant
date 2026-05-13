bb8bb3197ba3abfad74c4d716ff3910b6fa90a82
Tania <taniaadidukh@gmail.com>
2026-05-13 22:23:46 +0300

    Add files via upload

--- booking.php
+++ booking.php
@@ -1,19 +1,19 @@
 <?php
-// Налаштування підключення (використовуємо твої дані з Workbench)
+// Цей рядок виправляє "каракулі"
+header('Content-Type: text/html; charset=utf-8');
+
 $servername = "10.211.55.4"; 
 $username = "taniad";        
-$password = "380676068565"; // пароль, у Workbench
-$dbname = "meat_and_fire";   
+$password = "380676068565"; 
+$dbname = "restaurant_db";   
 
-// Створюємо з'єднання
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Перевірка з'єднання
 if ($conn->connect_error) {
-    die("Помилка підключення: " . $conn->connect_error);
+    // Якщо сервер вимкнений, ми все одно покажемо гарний текст для звіту
+    die("<h2 style='color: red; font-family: sans-serif;'>Помилка підключення до бази. Перевір, чи запущена Ubuntu!</h2>");
 }
 
-// Отримуємо дані з форми
 $f_name = $_POST['first_name'];
 $l_name = $_POST['last_name'];
 $email = $_POST['email'];
@@ -21,20 +21,17 @@ $comment = $_POST['user_comment'];
 $phone = $_POST['phone'];
 $date = $_POST['booking_date'];
 
-// SQL-запит на додавання запису в нову таблицю
 $sql = "INSERT INTO web_bookings (first_name, last_name, email, comment, phone, booking_date)
 VALUES ('$f_name', '$l_name', '$email', '$comment', '$phone', '$date')";
 
-
 if ($conn->query($sql) === TRUE) {
-    echo "<div style='background: #111; color: white; padding: 50px; text-align: center; font-family: Montserrat;'>
+    echo "<div style='background: #111; color: white; padding: 50px; text-align: center; font-family: sans-serif;'>
             <h2 style='color: #FF7900;'>Бронювання успішне!</h2>
-            <p>Ваші дані збережені в базі даних ресторану.</p>
+            <p>Дані передано в MySQL (web_bookings).</p>
             <a href='fuulfirsttry.html' style='color: #FF7900;'>Повернутися на сайт</a>
           </div>";
 } else {
-    echo "Помилка: " . $sql . "<br>" . $conn->error;
+    echo "Помилка: " . $conn->error;
 }
-
 $conn->close();
 ?>
